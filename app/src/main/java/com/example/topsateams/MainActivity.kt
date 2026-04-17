9addef4e5284d84a1c24e345db784a76f0693eb7
Shaunnybwoyy <shaunmwanza31@gmail.com>
2026-04-17 19:14:44 +0200

    added variable

--- app/src/main/java/com/example/topsateams/MainActivity.kt
+++ app/src/main/java/com/example/topsateams/MainActivity.kt
@@ -1,37 +1,28 @@
 package com.example.topsateams
 
-import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+fun getShortestString(arr: List<String>): String {
+    var shortest = arr[0]
 
-class MainActivity : AppCompatActivity() {
-
-    //created array to hold up 5 soccer teams
-
-
-    val teams = arrayOf<String>("Mam Sundowns FC" ,"Bidwest Wits","Orlando Pirates","Kaizer Chiefs"
-                               , "Cape Town City FC")
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        // get the teams text view
-        val TeamsTxt = findViewById<TextView>(R.id.teamsDisplayTxt)
-        var teamsDisplayTxt = ""
-        var count = 0
-
-        //calling logArrValues function
-        logArrValues(teams)
-        logArrValues
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    for (team in arr) {
+        if (team.length < shortest.length) {
+            shortest = team
         }
     }
+
+    return shortest
+}
+
+fun main() {
+    val topSoccerTeams = listOf(
+        "Real Madrid",
+        "FC Barcelona",
+        "Manchester City",
+        "Bayern Munich",
+        "Liverpool",
+        "Chelsea",
+        "PSG"
+    )
+
+    val shortestTeam = getShortestString(topSoccerTeams)
+    println("Shortest team name: $shortestTeam")
 }
